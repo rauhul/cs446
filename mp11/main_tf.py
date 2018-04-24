6fe5f3a90bfa2e57a240df44cda8de970b764d88
rauhul <rauhul.varma@gmail.com>
2018-04-24 18:34:20 -0500

    mp11 inital

--- mp11/main_tf.py
+++ mp11/main_tf.py
@@ -7,9 +7,8 @@ import numpy as np
 import matplotlib.pyplot as plt
 from models.gan import Gan
 
-
-def train(model, mnist_dataset, learning_rate=0.0005, batch_size=16,
-          num_steps=5000):
+def train(model, mnist_dataset, learning_rate=0.0001, batch_size=32,
+          num_steps=800, nlatent=2, epoch=0):
     """Implements the training loop of stochastic gradient descent.
 
     Performs stochastic gradient descent with the indicated batch_size and
@@ -22,10 +21,62 @@ def train(model, mnist_dataset, learning_rate=0.0005, batch_size=16,
         batch_size(int): batch size used for training.
         num_steps(int): Number of steps to run the update ops.
     """
-    for step in range(0, num_steps):
-        batch_x, _ = mnist_dataset.train.next_batch(batch_size)
-        batch_z = np.random.uniform(-1,1,[batch_size,2])
-        # Train generator and discriminator
+    d_iters = 1
+    g_iters = 8
+    for step in range(num_steps):
+        for _ in range(d_iters):
+            batch_x, _ = mnist_dataset.train.next_batch(batch_size)
+            model.session.run(
+                model.update_d_op,
+                feed_dict={
+                    model.x_placeholder: batch_x,
+                    model.z_placeholder: np.random.uniform(-1,1,[batch_size,nlatent]),
+                    model.learning_rate_placeholder: learning_rate
+                }
+            )
+
+        for _ in range(g_iters):
+            batch_x, _ = mnist_dataset.train.next_batch(batch_size)
+            model.session.run(
+                model.update_g_op,
+                feed_dict={
+                    model.x_placeholder: batch_x,
+                    model.z_placeholder: np.random.uniform(-1,1,[batch_size,nlatent]),
+                    model.learning_rate_placeholder: learning_rate
+                }
+            )
+
+        if step % 100 == 0:
+            summary = model.summary_op
+            batch_x, _ = mnist_dataset.train.next_batch(batch_size)
+            summary = model.session.run(
+                model.summary_op,
+                feed_dict={
+                    model.x_placeholder: batch_x,
+                    model.z_placeholder: np.random.uniform(-1,1,[batch_size,nlatent]),
+                    model.learning_rate_placeholder: learning_rate
+                }
+            )
+            model.writer.add_summary(summary, step + epoch * num_steps)
+
+def generate_image(model, nlatent=2):
+    n_images = 5
+    x_z = np.linspace(-1, 1, n_images)
+    y_z = np.linspace(-1, 1, n_images)
+
+    out = np.empty((28*n_images, 28*n_images))
+    for x_idx, x in enumerate(x_z):
+        for y_idx, y in enumerate(y_z):
+            z = []
+            if nlatent == 2:
+                z = np.array([[y, x]], dtype=np.float32)
+            else:
+                z = np.array(np.random.uniform(-1,1,[1,nlatent]), dtype=np.float32)
+            z = tf.convert_to_tensor(z, np.float32)
+            img = model._generator(z, reuse=True).eval()
+            out[x_idx*28:(x_idx+1)*28,
+                y_idx*28:(y_idx+1)*28] = img.reshape((28, 28))
+    plt.imsave('latent_space_gan.png', out, cmap="gray")
 
 
 def main(_):
@@ -35,13 +86,19 @@ def main(_):
     """
     # Get dataset.
     mnist_dataset = input_data.read_data_sets('MNIST_data', one_hot=True)
+    nlatent = 2
 
     # Build model.
-    model = Gan()
+    model = Gan(nlatent=nlatent)
 
-    # Start training
-    train(model, mnist_dataset)
+    # Start training.
+    for i in range(10):
+        print("epoch", i)
+        train(model, mnist_dataset, nlatent=nlatent, epoch=i)
 
+        # Plot out latent space, for +/- 1.
+        print("generating image", i)
+        generate_image(model, nlatent=nlatent)
 
 if __name__ == "__main__":
     tf.app.run()
--- mp11/models/gan.py
+++ mp11/models/gan.py
@@ -3,9 +3,6 @@
 import numpy as np
 import tensorflow as tf
 
-from tensorflow import contrib
-from tensorflow.contrib import layers
-
 class Gan(object):
     """Adversary based generator network.
     """
@@ -16,8 +13,7 @@ class Gan(object):
             ndims(int): Number of dimensions in the feature.
             nlatent(int): Number of dimensions in the latent space.
         """
-
-        self._ndims = ndims
+        self._ndims   = ndims
         self._nlatent = nlatent
 
         # Input images
@@ -26,10 +22,13 @@ class Gan(object):
         # Input noise
         self.z_placeholder = tf.placeholder(tf.float32, [None, nlatent])
 
+        # Input learning rate
+        self.learning_rate_placeholder = tf.placeholder(tf.float32)
+
         # Build graph.
         self.x_hat = self._generator(self.z_placeholder)
-        y_hat = self._discriminator(self.x_hat)
-        y = self._discriminator(self.x_placeholder, reuse=True)
+        y_hat      = self._discriminator(self.x_hat)
+        y          = self._discriminator(self.x_placeholder, reuse=True)
 
         # Discriminator loss
         self.d_loss = self._discriminator_loss(y, y_hat)
@@ -38,11 +37,22 @@ class Gan(object):
         self.g_loss = self._generator_loss(y_hat)
 
         # Add optimizers for appropriate variables
+        self.update_d_op = tf.train.AdamOptimizer(learning_rate=self.learning_rate_placeholder).minimize(
+            self.d_loss,
+            var_list=[var for var in tf.global_variables() if "discriminator" in var.name]
+        )
+        self.update_g_op = tf.train.AdamOptimizer(learning_rate=self.learning_rate_placeholder).minimize(
+            self.g_loss,
+            var_list=[var for var in tf.global_variables() if "generator" in var.name]
+        )
+
+        # Tensorboard
+        self.writer = tf.summary.FileWriter("/Users/rauhul/Desktop/tensorboard_logs/", graph=tf.get_default_graph())
+        self.summary_op = tf.summary.merge_all()
 
         # Create session
         self.session = tf.InteractiveSession()
         self.session.run(tf.global_variables_initializer())
-
 
     def _discriminator(self, x, reuse=False):
         """Discriminator block of the network.
@@ -52,14 +62,15 @@ class Gan(object):
             reuse (Boolean): re use variables with same name in scope instead of creating
               new ones, check Tensorflow documentation
         Returns:
-            y (tf.Tensor): Scalar output prediction D(x) for true vs fake image(None, 1). 
+            y (tf.Tensor): Scalar output prediction D(x) for true vs fake image(None, 1).
               DO NOT USE AN ACTIVATION FUNCTION AT THE OUTPUT LAYER HERE.
-
         """
         with tf.variable_scope("discriminator", reuse=reuse) as scope:
-            y = None
-            return y
-
+            d_full_1 = tf.nn.dropout(tf.layers.dense(x,            512, activation=tf.nn.leaky_relu, kernel_initializer=tf.random_normal_initializer(stddev=0.02)), 0.2)
+            d_full_2 = tf.nn.dropout(tf.layers.dense(d_full_1,     256, activation=tf.nn.leaky_relu, kernel_initializer=tf.random_normal_initializer(stddev=0.02)), 0.2)
+            d_batch_norm = tf.layers.batch_normalization(d_full_2)
+            d_full_3 = tf.layers.dense(d_batch_norm, 1, activation=None)
+            return d_full_3
 
     def _discriminator_loss(self, y, y_hat):
         """Loss for the discriminator.
@@ -69,11 +80,14 @@ class Gan(object):
             y_hat (tf.Tensor): The output tensor of the discriminator for fake images of dimension (None, 1).
         Returns:
             l (tf.Scalar): average batch loss for the discriminator.
-
         """
-        l = 0
-        return l
+        real = tf.reduce_mean(tf.nn.sigmoid_cross_entropy_with_logits(logits=y,     labels=tf.ones_like(y)))
+        fake = tf.reduce_mean(tf.nn.sigmoid_cross_entropy_with_logits(logits=y_hat, labels=tf.zeros_like(y_hat)))
 
+        tf.summary.scalar('discriminator_cross_entropy_real', real)
+        tf.summary.scalar('discriminator_cross_entropy_fake', fake)
+
+        return real + fake
 
     def _generator(self, z, reuse=False):
         """From a sampled z, generate an image.
@@ -81,14 +95,15 @@ class Gan(object):
         Args:
             z(tf.Tensor): z from _sample_z of dimension (None, 2).
             reuse (Boolean): re use variables with same name in scope instead of creating
-              new ones, check Tensorflow documentation 
+              new ones, check Tensorflow documentation
         Returns:
             x_hat(tf.Tensor): Fake image G(z) (None, 784).
         """
         with tf.variable_scope("generator", reuse=reuse) as scope:
-            x_hat = None
-            return x_hat
-
+            g_layer_1 = tf.nn.dropout(tf.layers.dense(z,         256, activation=tf.nn.leaky_relu, kernel_initializer=tf.random_normal_initializer(stddev=0.02)), 0.2)
+            g_layer_2 = tf.nn.dropout(tf.layers.dense(g_layer_1, 512, activation=tf.nn.leaky_relu, kernel_initializer=tf.random_normal_initializer(stddev=0.02)), 0.2)
+            g_layer_3 = tf.nn.dropout(tf.layers.dense(g_layer_2, 784, activation=tf.nn.leaky_relu, kernel_initializer=tf.random_normal_initializer(stddev=0.02)), 0.2)
+            return g_layer_3
 
     def _generator_loss(self, y_hat):
         """Loss for the discriminator.
@@ -97,7 +112,9 @@ class Gan(object):
             y_hat (tf.Tensor): The output tensor of the discriminator for fake images of dimension (None, 1).
         Returns:
             l (tf.Scalar): average batch loss for the discriminator.
-
         """
-        l = 0
-        return l
+
+        # move towards 1
+        g_loss = tf.reduce_mean(tf.nn.sigmoid_cross_entropy_with_logits(logits=y_hat, labels=tf.ones_like(y_hat)))
+        tf.summary.scalar('generator_cross_entropy', g_loss)
+        return g_loss
